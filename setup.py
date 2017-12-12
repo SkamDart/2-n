c1be96308adfa861918637596910106d2ae1cbbd
Cameron Dart <cdart2@illinois.edu>
2017-12-11 21:43:13 -0600

    foobar

--- setup.py
+++ setup.py
@@ -3,20 +3,9 @@ try:
 except ImportError:
     from distutils.core import setup
 
-import unittest
-
-def test_suite():
-    """
-    """
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
-    return test_suite
-
 setup(name='2pyn',
       version='1.0',
       description='2048 AI',
       author='Cameron Dart',
-      author_email='cdart2@illinois.edu',
-      py_modules=['app', 'game'],
-      test='tests'
-      )
+      author_email='cdart2@illinois.edu'
+     )
